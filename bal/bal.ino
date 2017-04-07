3c97cdfc41f5d71daaa6b0def591ec3d7b7d3e19
raphael letendu <raphael.letendu@gmail.com>
2017-04-07 16:40:32 +0200

    BAL update

--- bal/bal.ino
+++ bal/bal.ino
@@ -1,12 +1,12 @@
 #include "LowPower.h"
+#include <EnableInterrupt.h>
+#include "DallasTemperaturePu.h"
+#include <EEPROM.h>
+
 
 //#define OREGON_DEBUG
 #define OREGON_SRC_INCLUDE
 #include "oregon/oregon.hpp"
-
-
-#include <EnableInterrupt.h>
-
 
 #define X10
 #ifdef X10
@@ -15,7 +15,6 @@
 #endif
 
 //#define DEBUG
-
 #ifdef DEBUG
 #ifndef DEBUG_PRINTER
 #define DEBUG_PRINTER Serial
@@ -27,23 +26,56 @@
 #define DEBUG_PRINTLN(...) {}
 #endif
 
-#define TX_POWER 4
-#define TX_PIN   3
-#define DS18B20_PIN 0
-#define WAKEUP_TIME 5*60/8
-#define DOOR_PIN  1
+#define MAIL_METER_ID              12
+#define MAIL_HOUSE_CODE            'B'
+#define MAIL_UNIT_CODE             1
+#define TEMPERATURE_CHANNEL        0x01
+#define TEMPERATURE_ID             0xBA
 
-#include "DallasTemperaturePu.h"
+// Pins allocation
+#define TX_POWER_PIN               4
+#define TX_DATA_PIN                3
+#define DS18B20_PIN                0
+#define FRONT_DOOR_PIN             1
+#define BACK_DOOR_PIN              2
+
+#define TEMPERATURE_WAKEUP_TIME    20 // Temperature update period in minutes
+#define MIN_TIME_MAIL_REFRESH_MS   100
+#define MIN_VCC_OK_MV              2800
+
+#define EE_ADR_MAIL_METER_ID              0x00
+#define EE_ADR_TEMPERATURE_CHANNEL        0x01
+#define EE_ADR_TEMPERATURE_ID             0x02
+
+#define WAKEUP_COUNT_TIME                 TEMPERATURE_WAKEUP_TIME*60/8
+
+
 DallasTemperature DS18B20(DS18B20_PIN);
-
-
-x10rf myx10 = x10rf(TX_PIN, 0, 5);
+x10rf myx10 = x10rf(TX_DATA_PIN, 0, 5);
 Oregon rf_sender = Oregon();
-bool wakeup = true;
-uint8_t wakeup_count = 0;
+bool mail_update_request = true;
+uint16_t wakeup_count = WAKEUP_COUNT_TIME;
 uint16_t count_mail = 0;
+unsigned long time;
+unsigned long time_last_mail_update = 0;
+uint8_t mail_meter_id = MAIL_METER_ID;
+char mail_house_code = MAIL_HOUSE_CODE;
+char mail_unit_code = MAIL_UNIT_CODE;
 
+uint8_t temperature_channel = TEMPERATURE_CHANNEL;
+uint8_t temperature_id = TEMPERATURE_ID;
 
+void radio_power_on(void)
+{
+  digitalWrite( TX_POWER_PIN , HIGH );
+  digitalWrite( TX_DATA_PIN , HIGH );
+}
+
+void radio_power_off(void)
+{
+  digitalWrite( TX_POWER_PIN , LOW );
+  digitalWrite( TX_DATA_PIN , LOW );
+}
 
 bool update_ds18b20_temperature_raw(int *temp)
 {
@@ -55,6 +87,7 @@ bool update_ds18b20_temperature_raw(int *temp)
   DS18B20.requestTemperatures();
   if ( DS18B20.getDeviceCount() == 0) {
     DEBUG_PRINTLN("No Sensor Found");
+    return false;
   }
   DS18B20.getAddress(deviceAddress, 0);
 
@@ -65,72 +98,130 @@ bool update_ds18b20_temperature_raw(int *temp)
     DEBUG_PRINTLN("Failed reading DS18B20!");
     return false;
   }
-  DEBUG_PRINT("DS18B20 Temperature (1/128C): "); DEBUG_PRINTLN(t);
+  DEBUG_PRINT(F("DS18B20 Temperature (1/128C): ")); DEBUG_PRINTLN(t);
   *temp = t;
+  return true;
 }
 
-void interruptFunction() {
-  digitalWrite( TX_POWER , HIGH );
-   digitalWrite( TX_PIN , HIGH );
+void send_mail_count_update(void )
+{
+  radio_power_on();
+  // meter update
+  myx10.send_meter(mail_meter_id, count_mail);
+  // switch update
+  if (count_mail > 0) {
+    myx10.send_switch(mail_house_code, mail_unit_code, ON);
+  } else {
+    myx10.send_switch(mail_house_code, mail_unit_code, OFF);
+  }
+  radio_power_off();
+  time_last_mail_update = millis();
+}
 
-  count_mail++;
-  myx10.send_meter(12, count_mail);
-  digitalWrite( TX_POWER , LOW );
-   digitalWrite( TX_PIN , LOW );
+void front_door_interupt() {
+  time = millis();
+  if ( abs(time - time_last_mail_update) > MIN_TIME_MAIL_REFRESH_MS ) {
+    count_mail++;
+    mail_update_request = true;
+  }
+}
+void back_door_interupt() {
+  time = millis();
+  if ( abs(time - time_last_mail_update) > MIN_TIME_MAIL_REFRESH_MS ) {
+    count_mail = 0;
+    mail_update_request = true;
+  }
 }
 
 
 void setup()
 {
+  uint8_t i;
+  i = EEPROM.read(EE_ADR_MAIL_METER_ID);
+  if (i != 0xFF) {
+    mail_meter_id = i;
+  }
+  i = EEPROM.read(EE_ADR_TEMPERATURE_CHANNEL);
+  if (i != 0xFF) {
+    temperature_channel = i;
+  }
+  i = EEPROM.read(EE_ADR_TEMPERATURE_ID);
+  if (i != 0xFF) {
+    temperature_id = i;
+  }
 
-  pinMode( DOOR_PIN  , INPUT_PULLUP );
+  // Init interupt pins detection
+  // Back door mail box detection is micro swtich Normaly Open
+  // Opening the door will close the switch
+  pinMode( BACK_DOOR_PIN  , INPUT_PULLUP );
+  enableInterrupt(BACK_DOOR_PIN, back_door_interupt, FALLING);
+  // Front door mail box detection is micro reelswtich Normaly Close
+  // Opening the door will open the switch
+  pinMode( FRONT_DOOR_PIN  , INPUT ); //! Caution no internal pull up, external one only !
+  enableInterrupt(FRONT_DOOR_PIN, front_door_interupt, RISING);
 
-  enableInterrupt(DOOR_PIN, interruptFunction, FALLING);
-
-  digitalWrite( TX_POWER , LOW );
-  pinMode( TX_POWER  , OUTPUT );
-
-  digitalWrite( TX_POWER , HIGH );
-  delay(100);
-  digitalWrite( TX_POWER , LOW );
-  delay(100);
-  digitalWrite( TX_POWER , HIGH );
-  delay(100);
-  digitalWrite( TX_POWER , LOW );
+  // Init Radio Vcc pin
+  digitalWrite( TX_POWER_PIN , LOW );
+  pinMode( TX_POWER_PIN  , OUTPUT );
+  // Notify Startup
+  for (i = 0; i < 10; i++) {
+    radio_power_on();
+    delay(100);
+    radio_power_off();
+    delay(100);
+  }
   myx10.begin();
-
 }
 
 void loop()
 {
-
-  int16_t temp;
+  int16_t raw_temperature;
   byte state;
-  if (++wakeup_count >= WAKEUP_TIME) {
-    wakeup = true;
+  bool battery_status;
+
+  if (mail_update_request) {
+    send_mail_count_update();
+    mail_update_request = false;
+    delay(MIN_TIME_MAIL_REFRESH_MS);
   }
-
-  if (wakeup) {
-    digitalWrite( TX_PIN , HIGH );
-    digitalWrite( TX_POWER , HIGH );
-
-    rf_sender.begin(TX_PIN);
-#if 1
-    update_ds18b20_temperature_raw(&temp);
-    rf_sender.send_temperature_from_ds18(0x20, 0xBA, 0, 1);
-#else
-    delay(1000);
-#endif
-    wakeup = false;
+  if (++wakeup_count >= WAKEUP_COUNT_TIME) {
+    if (getVCC() >= MIN_VCC_OK_MV) {
+      battery_status = true;
+    } else {
+      battery_status = false;
+    }
+    radio_power_on();
+    rf_sender.begin(TX_DATA_PIN);
+    if ( update_ds18b20_temperature_raw(&raw_temperature) ) {
+      rf_sender.send_temperature_from_ds18(temperature_channel, temperature_id, raw_temperature, battery_status);
+    }
     wakeup_count = 0;
-
-    digitalWrite( TX_POWER , LOW );
-    digitalWrite( TX_PIN , LOW );
+    radio_power_off();
   }
-
   LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF) ;
+}
 
 
+
+int getVCC() {
+#if defined(__AVR_ATtiny85__)
+  ADMUX = _BV(MUX3) | _BV(MUX2); // For ATtiny85
+#elif defined(__AVR_ATmega1284P__)
+  ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);  // For ATmega1284
+#else
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);  // For ATmega328
+#endif
+  delay(2);
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA, ADSC));
+  uint8_t low = ADCL;
+  unsigned int val = (ADCH << 8) | low;
+  //discard previous result
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA, ADSC));
+  low = ADCL;
+  val = (ADCH << 8) | low;
+  return ((long)1024 * 1100) / val;
 }
 
 /*
